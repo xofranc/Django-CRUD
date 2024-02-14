f6e2fbf726329e2075ac4d4b72cc988c151bfb21
Santiago Vasquez Franco <santiagop200101@gmail.com>
2024-02-15 00:23:39 +0100

    push

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 
 
-#creacion de Usuarios
+# creacion de Usuarios
+
+
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
