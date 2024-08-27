eeea8d94744c0bfa6e34409437a499170d6a1394
santiagofrancco3 <80358628+xofranc@users.noreply.github.com>
2024-08-26 22:06:51 -0500

    actualizaciones

--- historial/models.py
+++ historial/models.py
@@ -3,14 +3,21 @@ from django.db import models
 # Create your models here.
 
 # define las propiedades del paciente
-class Paciente(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    fecha_nacimiento = models.DateField()
-    sexo = models.CharField(max_length=1, choices=[('M', 'Masculino'), ('F', 'Femenino'), ('O', 'Otro no especificado')])
-    direccion = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=10)
-    email = models.EmailField()
+# class Paciente(models.Model):
+#     nombre = models.CharField(max_length=50)
+#     apellido = models.CharField(max_length=50)
+#     fecha_nacimiento = models.DateField()
+#     sexo = models.CharField(max_length=1, choices=[('M', 'Masculino'), ('F', 'Femenino'), ('O', 'Otro no especificado')])
+#     direccion = models.CharField(max_length=100)
+#     telefono = models.CharField(max_length=10)
+#     email = models.EmailField()
+#
+#     def __str__(self):
+#         return f'{self.apellido}, {self.nombre}'
 
-    def __str__(self):
-        return f'{self.apellido}, {self.nombre}'
+class Paciente(models.Model):
+    name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female'), ('O', 'Other')])
+    address = models.CharField(max_length=120)
+    telephone = models.CharField(max_length=120)
